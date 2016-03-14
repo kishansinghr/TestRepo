3a51cd98803facdb3ee5edc6f8481139fad3bff7
kishan <kishan@administrator>
2016-03-14 17:09:11 +0530

    Initial commit

---
+++ public_html/scripts/app.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+angular.module('myApp').controller('MainController',['$scope',function($function){
+    $scope.message = "hello world";    
+}]);
+
+
